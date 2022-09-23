8b20baf5a554aa49058f2f7a35e468de9baa0396
Wen <kairong@Kais-MacBook-Pro.local>
2022-09-23 13:07:19 -0500

    saving

--- leetcode/problems/test.js
+++ leetcode/problems/test.js
@@ -1,11 +1,9 @@
 // var canMakeArithmeticProgression = function(arr) {
 //     arr.sort((a,b)=> a-b);
 
-const { Console } = require("console");
-const { type } = require("os");
-const { isString } = require("util");
-
-
+// const { Console } = require("console");
+// const { type } = require("os");
+// const { isString } = require("util");
 
 //     for (i=0;i<arr.length;i++) {
 //         if (arr.length <= 2) {
@@ -33,7 +31,6 @@ const { isString } = require("util");
 // };
 // console.log(canMakeArithmeticProgression([2,0,4]));
 
-
 // console.log(canMakeArithmeticProgression([-13,-17,-8,-10,-20,2,3,-19,2,-18,-5,7,-12,18,-17,12,-1]));
 // console.log(
 //     "hello"
@@ -57,8 +54,4 @@ const { isString } = require("util");
 
 // preorder([1,null,3,2,4,null,5,6])
 
-
-
-
-
-console.log(nextGreaterElement([1, 3, 5, 2, 4], [6, 5, 4, 3, 2, 1, 7]));
+// console.log(nextGreaterElement([1, 3, 5, 2, 4], [6, 5, 4, 3, 2, 1, 7]));
