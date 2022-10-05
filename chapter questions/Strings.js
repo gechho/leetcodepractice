103c86db92b346dcddf8982fc8ef3d61b3c48f2b
W <juanzi.dd@gmail.com>
2022-10-05 10:22:58 -0500

    Strings Day2

--- chapter questions/Strings.js
+++ chapter questions/Strings.js
@@ -1,3 +1,4 @@
+// Day1
 // Reverse String
 /*
 Write a function that reverses a string. The input string is given as an array of characters s.
@@ -69,3 +70,68 @@ Assume the environment does not allow you to store 64-bit integers (signed or un
 };
 
 console.log(reverse(-13425465430));
+
+
+// Day2
+// First Unique Character in a String
+/* Given a string s, find the first non-repeating character in it and return its index. 
+If it does not exist, return -1.
+*/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var firstUniqChar = function (s) {
+    let Count = {};
+
+    for (i = 0; i < s.length; i++) {
+        if (Count[s[i]]) {
+            Count[s[i]]++
+        }
+        else {
+            Count[s[i]] = 1;
+        }
+    }
+
+    let keys = [];
+    for (const [key, value] of Object.entries(Count)) {
+        if (Count[key] == 1) {
+            keys.push(key);
+        }
+    }
+    return s.indexOf(keys[0]);
+};
+
+
+// OR
+let firstUniqChar = function(s) {
+    let map=new Map();
+    for(let x of s){
+        if(map.has(x)){
+            map.set(x,map.get(x)+1);
+        }
+        else{
+            map.set(x,0);
+        }
+    }
+    console.log(map,'map');
+    for(let[k,v] of map){
+        if(v===0){
+            let val=s.indexOf(k);
+            return val;
+        }
+    }    
+};
+
+
+var firstUniqChar = function(s) {
+    let count = {};
+    for (let eachItem of s) {
+        count[eachItem] = (count[eachItem] || 0) + 1;
+    }
+    for (let e in count){
+        if (count[e] === 1) return s.indexOf(e)
+    }
+    return -1;
+};
+console.log(firstUniqChar("loewivesdfsdfadrgleetcode"));
