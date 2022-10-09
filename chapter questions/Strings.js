5b0acb5f33f46aa36d350bc18e0f2e428e93d64f
W <juanzi.dd@gmail.com>
2022-10-08 22:15:26 -0500

    String Final

--- chapter questions/Strings.js
+++ chapter questions/Strings.js
@@ -135,3 +135,198 @@ var firstUniqChar = function(s) {
     return -1;
 };
 console.log(firstUniqChar("loewivesdfsdfadrgleetcode"));
+
+
+
+// Valid Anagram
+/* Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or
+phrase, typically using all the original letters exactly once.
+*/
+
+var isAnagram = function(s, t) {
+
+    return t.split("").sort().join()==s.split("").sort().join() ? true:false;
+};
+console.log(isAnagram("anagram",
+"nagaram"))
+
+
+//  Valid Palindrome
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, 
+it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+*/
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ let n = arr.length;
+
+ var isPalindrome = function (s) {
+     let regex = /[A-Z]|[a-z]|[0-9]/g;
+     let arr = s.toLowerCase().match(regex);
+     let result = true;
+     if (arr != null) {
+         let n = arr.length;
+ 
+         for (i = 0; i <= Math.floor(n / 2); i++) {
+             if (arr[i] !== arr[n - 1 - i]) {
+                 console.log(arr[i], i, arr[n - 1 - i], n - 1 - i);
+                 result = false;
+             }
+         }
+     }
+     return result;
+ };
+ console.log(isPalindrome("1A man, a plan, a canal: Panama0"));
+
+ // String to Integer (atoi)
+/*
+Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+The algorithm for myAtoi(string s) is as follows:
+
+Read in and ignore any leading whitespace.
+Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. 
+This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. 
+Change the sign as necessary (from step 2).
+If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. 
+Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+Return the integer as the final result.
+Note:
+
+Only the space character ' ' is considered a whitespace character.
+Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function (s) {
+
+    let nums = [];
+    let arr = s.trim().split("");
+    let final = -1;
+
+    if (arr[0] === '-' || arr[0] === '+') {
+        nums.push(arr[0]);
+        arr.shift();
+        console.log('+/- arr: ' + arr, 'nums: ' + nums);
+    }
+
+
+
+    for (i = 0; i < arr.length; i++) {
+
+        if ((arr[i] <= 9)&&(arr[i] !=" ")) {
+            nums.push(arr[i])
+            console.log(' nums: ' + nums, 'arr[i]: ' + arr[i], 'arr: ' + arr);
+
+        }
+        else {
+            if (i == 0) {
+                console.log('first digit is no-digit: ' + arr[0]);
+                return final =0;
+                break;
+            }
+            else {
+                console.log('hi,no-digit occurs');
+                break;
+            }
+
+        }
+
+    }
+
+    final = Number(nums.join(""));
+    console.log(nums,final);
+    if (nums == '+'||nums =='-') {
+        final = 0;
+    } 
+    if (Math.pow(-2, 31) > final) {
+
+        final = Math.pow(-2, 31);
+    }
+    else if (final > Math.pow(2, 31) - 1) {
+
+        final = Math.pow(2, 31) - 1;
+    }
+    return final;
+};
+console.log(myAtoi("   +0 42"));
+
+
+
+// Implement strStr()
+/*
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle);
+};
+console.log(strStr("sadbutsad",
+"sad"));
+
+
+// Longest Common Prefix
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+*/
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ var longestCommonPrefix = function(strs) {
+    let strcom = " ";
+    let i = 0;
+    let result = true;
+    for (i=0;i<strs[0].length;i++){
+        let str =strs[0].substring(0,i+1);
+        strs.every((item)=>{      
+        return result = item.startsWith(str);       
+         })
+         if (result == true){
+            strcom = str;
+            console.log(result,i,strcom)
+         }
+
+
+         console.log(result,i,strcom);
+    }
+
+    return strcom;
+
+};
+
+
+//or
+let longestCommonPrefix = function(strs) {
+    if(!strs.length) {
+        return '';
+    }
+    for(let i = 0; i < strs[0].length; i++) {
+        for(let s of strs) {
+            if(s[i] !== strs[0][i]) {
+                return s.slice(0, i);
+            }
+        }
+    }
+    return strs[0];
+};
+console.log(longestCommonPrefix(["flower","flower","flower","flower"]));
